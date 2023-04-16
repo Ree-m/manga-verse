4b72d840f00a7396b1b730ea29f76bea1997f37b
Reem <reembsrat@gmail.com>
2023-04-17 02:08:01 +0300

    register post request working

--- app/api/auth/route.js
+++ app/api/auth/route.js
@@ -1,3 +1,24 @@
-async function register(){
-    
+import jwt from "jsonwebtoken";
+import User from "@/models/User";
+import connectMongo from "@/utils/connectMongo";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request) {
+  connectMongo();
+  const { username, email, password } = await request.json();
+  console.log("username", username, "email", email, "password", password);
+
+  try {
+    const user = await User.create({
+      username,
+      email,
+      password,
+    });
+    await user.save();
+    console.log("user added");
+    return NextResponse.json(user);
+  } catch (error) {
+    console.log("post register error", error);
+    return NextResponse.json(error);
+  }
 }
