57df78b39955fed1de7325ec6152e289e14bc008
Reem <reembsrat@gmail.com>
2023-04-17 01:01:35 +0300

    made login and register pages client components and and added value and onChange

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -1,13 +1,32 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  async function onSubmit(e) {
+    e.preventDefault();
+    console.log("submit");
+  }
+
   return (
-    <form>
-      <input type="email" placeholder="email" />
-      <input type="password" placeholder="password" />
+    <form onSubmit={onSubmit}>
+      <input
+        type="email"
+        value={email}
+        placeholder="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button>Login</button>
 
       <p>
-        Don't have an account?{" "}
+        Don't have an account?
         <button>
           <Link href="/auth/register">Create one</Link>
         </button>
--- app/auth/register/page.jsx
+++ app/auth/register/page.jsx
@@ -1,9 +1,35 @@
+"use client";
+import { setRequestMeta } from "next/dist/server/request-meta";
+import { useState } from "react";
+
 const RegisterPage = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  async function onSubmit(e) {
+    e.preventDefault();
+    console.log("register");
+  }
   return (
-    <form>
-      <input type="text" placeholder="username" />
-      <input type="email" placeholder="email" />
-      <input type="password" placeholder="password" />
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        value={username}
+        placeholder="username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="email"
+        value={email}
+        placeholder="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
 
       <button>Register</button>
     </form>
