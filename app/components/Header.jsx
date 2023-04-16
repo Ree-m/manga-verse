6a00cfd04ac4dd34322485ad8092f7d29aef7b8c
Reem <reembsrat@gmail.com>
2023-04-16 23:01:24 +0300

    made new auth path and login and register pages

---
+++ app/components/Header.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+const Header = () => {
+    return ( 
+        <div>
+            <h1>Mang App</h1>
+            <Link href="/login">Login</Link>
+
+        </div>
+     );
+}
+ 
+export default Header;
