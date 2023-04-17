288dec2c5e1dddda4ba42f3dc845526ab7435895
Reem <reembsrat@gmail.com>
2023-04-17 18:21:24 +0300

    changed manga/id page,bookmark working on button click,getting weird useEffect problem

--- app/mangas/[id]/page.jsx
+++ app/mangas/[id]/page.jsx
@@ -1,27 +1,53 @@
-import connectMongo from "@/utils/connectMongo";
-import Bookmark from "@/models/Bookmark";
+"use client";
+import { useEffect, useState } from "react";
 
 
-async function fetchManga(id) {
-  console.log("this is id", id);
-  const response = await fetch(`https://api.jikan.moe/v4/manga/${id}`);
-  const manga = await response.json();
-  return manga.data;
-}
-// add manga to bookmark
-async function addToBookmark(){
+const MangaPage = async ({ params: { id } }) => {
+  // const manga = await fetchManga(id);
+  const [bookmark, setBookmark] = useState("");
+  const [manga,setManga]=useState("")
+
+  // useEffect(()=>{
+  //   async function fetchManga() {
+  //     console.log("this is id", id);
+  //     const response = await fetch(`https://api.jikan.moe/v4/manga/${id}`);
+  //     const manga = await response.json();
+  //     setManga(manga)
+  //   }
+  //   fetchManga()
+  // },[id])
+
+  useEffect(() => {
+    fetch(`https://api.jikan.moe/v4/manga/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "this is data");
+        setManga(data);
+      });
+  }, []);
  
 
-}
-const MangaPage = async ({ params: { id } }) => {
-  const manga = await fetchManga(id);
-  console.log("this is single manga", manga);
+  // add manga to bookmark
+  async function addToBookmark(nameOfBookmark) {
+    console.log("start bookmark");
+    const response = await fetch(`http://localhost:3000/api/bookmark`, {
+      method: "POST",
+
+      body: JSON.stringify({ nameOfBookmark }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const bookmark = await response.json();
+    setBookmark(bookmark);
+    console.log("finish bookmark", bookmark);
+  }
 
   return (
     <div>
       <h1>{manga.title}</h1>
       <p>{manga.synopsis}</p>
-      <button onClick={addToBookmark}>Bookmark</button>
+      <button onClick={() => addToBookmark(manga.title)}>Bookmark</button>
     </div>
   );
 };
