6f1738abbe3cdda6d008b3093d60faaae3cb9670
Reem <reembsrat@gmail.com>
2023-04-21 16:28:29 +0300

    commit to savechanges for changing branch

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
-import { Inter } from "next/font/google";
+import { Inter, Noticia_Text } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 import Link from "next/link";
 
 export default function Home() {
+  const API_URL = process.env.API_URL;
   return (
     <main>
       <Link href="/mangas">Go to all mangas</Link>
