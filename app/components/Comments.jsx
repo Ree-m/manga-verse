c41bd6afdaa04483a9acebea5973acd836640b54
Reem <reembsrat@gmail.com>
2023-04-28 06:45:27 +0300

    added comment context

--- app/components/Comments.jsx
+++ app/components/Comments.jsx
@@ -1,28 +1,37 @@
 "use client";
-import { useEffect ,useState} from "react";
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
 const Comments = () => {
   const [comments, setComments] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     async function fetchComments() {
+      setLoading(true);
       const response = await fetch(`http://localhost:3000/api/comment`);
       const data = await response.json();
       console.log("comments data", data);
       setComments(data);
+      setLoading(false);
     }
     fetchComments();
-  },[]);
+  }, []);
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <div>
-      {
-        comments && comments.map((comment)=>(
-            <div key={comment._id}>
-                <p>{comment.commentText}</p>
-                <span>{comment.likes}</span>
-                <br/>
-                <span>{comment.userId}</span>
-            </div>
-        ))
-      }
+      {comments &&
+        comments.map((comment) => (
+          <div key={comment._id}>
+            <p>{comment.commentText}</p>
+            <span>{comment.likes}</span>
+            <br />
+            <span>{comment.userId}</span>
+            <button onClick={() => deleteComment()}>delete</button>
+          </div>
+        ))}
     </div>
   );
 };
---
+++ app/context/comment.js
@@ -0,0 +1,17 @@
+"use client";
+
+import { useContext, useState, createContext } from "react";
+
+const CommentContext = createContext([]);
+
+export const CommentContextProvider = ({ children }) => {
+  const [comments, setComments] = useState([]);
+
+  return (
+    <CommentContext.Provider value={[ comments, setComments]}>
+      {children}
+    </CommentContext.Provider>
+  );
+};
+
+export const useCommentContext = () => useContext(CommentContext);
