0912919e79d7b4f01b94ab74b8db54742becdad2
Reem <reembsrat@gmail.com>
2023-05-03 04:56:47 +0300

    added chapter and image models

---
+++ models/MangaChapters.js
@@ -0,0 +1,15 @@
+import { Schema, model, models } from "mongoose";
+import mongoose from "mongoose";
+
+const CommentSchema = new Schema(
+  {
+   
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Comment = models.Comment || model("Comment", CommentSchema);
+
+export default Comment;
