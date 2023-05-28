e5d98ad050e612549299935f9bea707eebaa027f
Reem <reembsrat@gmail.com>
2023-05-28 17:59:00 +0300

    commit for pr

--- app/components/ShowLess.jsx
+++ app/components/ShowLess.jsx
@@ -2,12 +2,11 @@
 import { useState } from "react";
 const ShowLess = ({ manga }) => {
   const [showMore, setShowMore] = useState(false);
-  const text = manga?.synopsis;
+  const text = manga?.synopsis?.replace(/\[Written by MAL Rewrite\]/g, '')
 
   return (
     <div className="mangaDescription">
-      {/* <p> {manga?.synopsis?.replace(/\[Written by MAL Rewrite\]/g, '')}</p> */}
-      <p>{showMore ? text : text?.substring(0, 400)}</p>
+      <p>{showMore ? text : text?.substring(0, 400)+`...`}</p>
       <button onClick={() => setShowMore(!showMore)}>
         {showMore ? "Show less" : "Show more"}
       </button>
