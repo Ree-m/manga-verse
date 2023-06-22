f189785223adb8a5025a6949082c9564eae8cb75
Ree-m <reembsrat@gmail.com>
2023-06-22 20:34:18 +0100

    paginate,readme,license

--- app/components/Paginate.jsx
+++ app/components/Paginate.jsx
@@ -39,37 +39,37 @@ import ReactPaginate from "react-paginate";
 import { useRouter } from "next/navigation";
 import "../styles/paginate.css";
 
-import {AiOutlineArrowRight} from "react-icons/ai"
-import {AiOutlineArrowLeft} from "react-icons/ai"
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 
 const Paginate = ({ pageCount, link }) => {
   const router = useRouter();
   return (
-    <ReactPaginate
-      activeClassName={"item active"}
-      breakClassName={"item break-me"}
-      breakLabel={"..."}
-      onPageChange={(data) => {
-        console.log(data.selected + 1, "data.selected");
-        router.push(`${link}/${data.selected + 1}`);
-      }}
-      containerClassName={"pagination"}
-      disabledClassName={"disabled-page"}
-      marginPagesDisplayed={2}
-      nextClassName={"item next"}
-      pageCount={pageCount}
-      pageClassName={"item pagination-page"}
-      pageRangeDisplayed={5}
-      previousClassName={"item previous"}
-      // previousLabel="< previous"
-      // nextLabel="next >"
-      previousLabel={<AiOutlineArrowLeft style={{ fontSize: 18, width: 150 }} />}
-      nextLabel={<AiOutlineArrowRight style={{ fontSize: 18, width: 150 }} />}
-
-
-
-
-    />
+    <div className="paginationContainer">
+      <ReactPaginate
+        activeClassName={"item active"}
+        breakClassName={"item break-me"}
+        breakLabel={"..."}
+        onPageChange={(data) => {
+          console.log(data.selected + 1, "data.selected");
+          router.push(`${link}/${data.selected + 1}`);
+        }}
+        containerClassName={"pagination"}
+        disabledClassName={"disabled-page"}
+        marginPagesDisplayed={2}
+        nextClassName={"item next"}
+        pageCount={pageCount}
+        pageClassName={"item pagination-page"}
+        pageRangeDisplayed={5}
+        previousClassName={"item previous"}
+        // previousLabel="< previous"
+        // nextLabel="next >"
+        previousLabel={
+          <AiOutlineArrowLeft style={{ fontSize: 18, width: 150 }} />
+        }
+        nextLabel={<AiOutlineArrowRight style={{ fontSize: 18, width: 150 }} />}
+      />
+    </div>
   );
 };
 
