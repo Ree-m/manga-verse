8e78aa7c768bb17e95a32e0d46f63ae94def9f37
Ree-m <reembsrat@gmail.com>
2023-07-30 08:21:23 +0300

    removed search, super expression error

--- app/search/[query]/[page]/page.jsx
+++ app/search/[query]/[page]/page.jsx
@@ -1,35 +1,35 @@
-import Link from "next/link";
-import MangaDetails from "@/app/components/MangaDetails";
-import Paginate from "@/app/components/Paginate";
+// import Link from "next/link";
+// import MangaDetails from "@/app/components/MangaDetails";
+// import Paginate from "@/app/components/Paginate";
 
 
 
-async function fetchSearchResults(query,page) {
-  console.log("start results fetch",query,page+1)
-  const response = await fetch(
-    `https://api.jikan.moe/v4/manga?q=${query}&limit=24&page=${page}`
-  );
-  const data = await response.json();
-  console.log("search results", data.data);
-  return data
-}
+// async function fetchSearchResults(query,page) {
+//   console.log("start results fetch",query,page+1)
+//   const response = await fetch(
+//     `https://api.jikan.moe/v4/manga?q=${query}&limit=24&page=${page}`
+//   );
+//   const data = await response.json();
+//   console.log("search results", data.data);
+//   return data
+// }
 
-const SearchResults = async ({ params }) => {
-  const query = params.query;
-  const page = + params.page;
-  console.log("search params", page + 1, query);
-  const data =await fetchSearchResults(query,page)
-  const results =data.data
-  console.log("results here",results,data)
-  const pageCount=data.pagination.last_visible_page
+// const SearchResults = async ({ params }) => {
+//   const query = params.query;
+//   const page = + params.page;
+//   console.log("search params", page + 1, query);
+//   const data =await fetchSearchResults(query,page)
+//   const results =data.data
+//   console.log("results here",results,data)
+//   const pageCount=data.pagination.last_visible_page
 
-  return (
-    <div>
-      <MangaDetails mangas={results}  />
-      <Paginate link={`/search/${query}`} pageCount={pageCount}/>
+//   return (
+//     <div>
+//       <MangaDetails mangas={results}  />
+//       <Paginate link={`/search/${query}`} pageCount={pageCount}/>
 
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
 
-export default SearchResults;
+// export default SearchResults;
