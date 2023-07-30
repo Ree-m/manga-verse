3547a7affd507853e5447fdaf3a7142ec0d9b97d
Ree-m <reembsrat@gmail.com>
2023-07-30 21:44:31 +0300

    added browserRevision

--- scraper/.puppeteerrc.cjs
+++ scraper/.puppeteerrc.cjs
@@ -3,8 +3,12 @@ const {join} = require('path');
 /**
  * @type {import("puppeteer").Configuration}
  */
+
+
 module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+  browserRevision: "117.0.5897.0"
+
 };
 
