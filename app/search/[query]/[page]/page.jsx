2e7bd229b262a5c695faf098d84d63402a27392f
Ree-m <reembsrat@gmail.com>
2023-07-30 13:00:45 +0300

    decoded query

--- app/search/[query]/[page]/page.jsx
+++ app/search/[query]/[page]/page.jsx
@@ -14,6 +14,7 @@ import styles2 from "app/styles/search.module.css";
 
 const SearchResults = ({ params }) => {
   const query = params.query;
+  const decodeQuery =decodeURIComponent(query)
   const page = +params.page;
 
   const [results,setResults]=useState([])
@@ -49,7 +50,7 @@ const SearchResults = ({ params }) => {
         <span>
           <AiOutlineDoubleRight className={styles.doubleArrowIcon} />
         </span>
-        <p>{query}</p>
+        <p>{decodeQuery}</p>
         <span>
           <AiOutlineDoubleRight className={styles.doubleArrowIcon} />
         </span>
