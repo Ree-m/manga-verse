11c338c882eddb0aa62d72b5691be2c1c68e2640
Ree-m <reembsrat@gmail.com>
2023-07-30 12:15:01 +0300

    fixed search error

--- app/search/[query]/[page]/page.jsx
+++ app/search/[query]/[page]/page.jsx
@@ -1,42 +1,61 @@
-// import Link from "next/link";
-// import MangaDetails from "@/app/components/MangaDetails";
-// import Paginate from "@/app/components/Paginate";
-// import { AiOutlineDoubleRight } from "react-icons/ai";
-// import styles from "app/styles/search.module.css"
+"use client" //react-paginate package doesnt work for server components
 
-// async function fetchSearchResults(query,page) {
-//   console.log("start results fetch",query,page+1)
-//   const response = await fetch(
-//     `https://api.jikan.moe/v4/manga?q=${query}&limit=24&page=${page}`
-//   );
-//   const data = await response.json();
-//   console.log("search results", data.data);
-//   return data
-// }
+import Link from "next/link";
+import MangaDetails from "@/app/components/MangaDetails";
+import Paginate from "@/app/components/Paginate";
+import { AiOutlineDoubleRight } from "react-icons/ai";
+import { useState ,useEffect} from "react";
+import Loading from "@/app/components/Loading";
+import styles from "app/styles/mangaDetialsPages.module.css";
+import styles2 from "app/styles/search.module.css";
 
-// const SearchResults = async ({ params }) => {
-//   const query = params.query;
-//   const page = + params.page;
-//   console.log("search params", page + 1, query);
-//   const data =await fetchSearchResults(query,page)
-//   const results =data.data
-//   console.log("results here",results,data)
-//   const pageCount=data.pagination.last_visible_page
 
-//   return (
-//     <div>
-{/* <div className={styles.top}>
-  <Link href={`/`}>Read Manga Online</Link>
-  <span>
-    <AiOutlineDoubleRight className={styles.doubleArrowIcon} />
-  </span>
-  <p>Search Results</p>
-</div>; */}
-//       <MangaDetails mangas={results}  />
-//       <Paginate link={`/search/${query}`} pageCount={pageCount}/>
 
-//     </div>
-//   );
-// };
 
-// export default SearchResults;
+const SearchResults = ({ params }) => {
+  const query = params.query;
+  const page = +params.page;
+
+  const [results,setResults]=useState([])
+  const [pageCount,setPageCount]=useState(null)
+  const [loading,setLoading]=useState(true)
+
+  async function fetchSearchResults(query, page) {
+    console.log("start results fetch", query, page + 1);
+    const response = await fetch(
+      `https://api.jikan.moe/v4/manga?q=${query}&limit=24&page=${page}`
+    );
+    const data = await response.json();
+    setResults(data.data)
+
+    setPageCount( data.pagination.last_visible_page)
+    setLoading(false)
+
+    console.log("search results", data.data);
+  }
+
+  useEffect(()=>{
+    fetchSearchResults(query,page)
+  },[])
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <div className={styles.mangaDetailsPage}>
+      <div className={styles2.top}>
+        <Link href={`/`}>Read Manga Online</Link>
+        <span>
+          <AiOutlineDoubleRight className={styles.doubleArrowIcon} />
+        </span>
+        <p>Search Results</p>
+      </div>
+      
+      <MangaDetails mangas={results} />
+      <Paginate link={`/search/${query}`} pageCount={pageCount} />
+    </div>
+  );
+};
+
+export default SearchResults;
