dc3c4fb4de144f1b46fa8fcc83fa887c04c0f389
Ree-m <reembsrat@gmail.com>
2023-08-11 16:28:02 +0300

    removed healthcheck page.jsx

--- app/healthCheck/page.jsx
+++ app/healthCheck/page.jsx
@@ -1,16 +1,16 @@
-const fetchStatus = async ()=>{
-    const response = await fetch(`/api/healthCheck`)
-    const data= await response.json()
-    console.log(data)
-    return data
-    }
-    const HealthCheck= async() => {
-        const data = await fetchStatus()
-      return (
-        <div>
-            hi
-        </div>
-      )
-    }
+// async function fetchStatus(){
+//     const response = await fetch(`/api/healthCheck`)
+//     const data= await response.json()
+//     console.log(data)
+//     return data
+//     }
+//     const HealthCheck= async() => {
+//         const data = await fetchStatus()
+//       return (
+//         <div>
+//             hi
+//         </div>
+//       )
+//     }
     
-    export default HealthCheck
+//     export default HealthCheck
