af87efb708a8f38fd924a4e09c0c8226464a7b41
Ree-m <reembsrat@gmail.com>
2023-08-28 21:58:13 +0300

    trying to fix puppeteer error

--- scraper/.puppeteer.cjs
+++ scraper/.puppeteer.cjs
@@ -8,6 +8,4 @@ const {join} = require('path');
 module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-  browserRevision: "115.0.5790.170"
-
 };
