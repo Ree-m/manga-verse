058835548b8ca1b272b5d85474fa33bbc533d5cb
Ree-m <reembsrat@gmail.com>
2023-08-28 21:42:43 +0300

    updated puppeteer config

--- scraper/.puppeteer.cjs
+++ scraper/.puppeteer.cjs
@@ -8,6 +8,6 @@ const {join} = require('path');
 module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-  browserRevision: "117.0.5897.0"
+  browserRevision: "115.0.5790.170"
 
 };
