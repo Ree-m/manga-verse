7af57c8ec722becd9a89fef5cae69d59e3bbf108
Ree-m <reembsrat@gmail.com>
2023-08-28 22:52:13 +0300

    updated css for chapter loader

--- app/components/LoadingChapter.jsx
+++ app/components/LoadingChapter.jsx
@@ -1,15 +1,23 @@
 import Link from "next/link";
-import styles from "../styles/loadingChapter.module.css"
+import styles from "../styles/loadingChapter.module.css";
 function LoadingChapter() {
   return (
     <div className={styles.loadingChapter}>
-        <p>
-        The bot is fetching your chapter ...</p>
-       <p> In the mean time, read the latest chapter of One piece <Link target="_blank" href="https://manga-verse-beta.vercel.app/mangas/13/chapter-1084">here</Link>
+      <div>
+        <p>The bot is fetching your chapter ...</p>
+        <p className={styles.content}>
+          
+          In the mean time, read the latest chapter of One piece{" "}
+          <Link
+            target="_blank"
+            href="https://manga-verse-beta.vercel.app/mangas/13/chapter-1084"
+          >
+            here
+          </Link>
         </p>
-
+      </div>
     </div>
-  )
+  );
 }
 
-export default LoadingChapter
+export default LoadingChapter;
